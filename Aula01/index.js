c97d5c884c9985b16ba4062d8b331df4d2a71cad
Sérgio de Souza Lima Sobrinho <sobrinhosergio00@gmail.com>
2024-10-14 20:55:07 -0300

    Adiciona conteúdo da aula01

---
+++ Aula01/index.js
@@ -0,0 +1,12 @@
+var nomeDaVariavel
+
+console.log(nomeDaVariavel)
+//nomeDaVariavel>10?console.log("Maior do que 10"):console.log("Menor ou igual a 10")
+//var a = nomeDaVariavel??10;
+//console.log(a);
+/*
+if(nomeDaVariavel>10) 
+    console.log("Maior do que 10")
+else 
+    console.log("Menor ou igual a 10")
+*/
